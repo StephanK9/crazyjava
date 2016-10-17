dd11663544c099373d2a51f615ea3ca14c040e89
Stephan Khoury and Viktor Grom <stephan@>
2016-10-17 16:41:04 -0700

    add first long java

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var add = function(sentence1) {
+  return sentence1;
+};
+var sentence1 = (prompt("Enter a sentence:"));
+
+var result = function( sentence1 ) {
+return (sentence1.charAt(0) + sentence1.slice(-1))
+};
+
+
+
+console.log(result((sentence1.charAt(0) + sentence1.slice(-1)).toUpperCase().split("").reverse("").join(""))) ;
+
+
+var sentence2 = (result((sentence1.charAt(0) + sentence1.slice(-1)).toUpperCase().split("").reverse("").join(""))) ;
+
+var sentence3 = function(sentence1 , sentence2) {
+return sentence1 + sentence2};
+
+
+console.log(sentence1 + sentence2)
